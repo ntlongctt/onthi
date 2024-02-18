82e23e7a6e0ca21d0005e30861bb6fb76663ed68
longnguyen <longnguyenctt@gmail.com>
2024-02-18 16:27:05 +0700

    Fix bug in login functionality

--- app/practice/page.tsx
+++ app/practice/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import data from "./data.json";
 
 type Question = {
   STT: string;
   "Nội dung câu hỏi": string;
-  "ĐÁP ÁN ĐÚNG": number;
+  "ĐÁP ÁN ĐÚNG": number | string;
   "Câu trả lời 1": string;
   "Câu trả lời 2": string;
   "Câu trả lời 3": string;
@@ -16,53 +16,81 @@ type Question = {
 };
 
 const Practice = () => {
-  const [questions, setQuestions] = useState<Question[]>(loadQuestion());
+  const [questions, setQuestions] = useState<Question[]>([]);
   console.log(questions);
 
   const [currentQuestIdx, setQuestionIdx] = useState(0);
 
   const result = questions.reduce((acc, q, idx) => {
-    const theCorrectAnswerArray = q["ĐÁP ÁN ĐÚNG"].toString().replaceAll(" ", "").split(",").map((i) => parseInt(i));
-    const correctBoolean = [false,false,false,false].map((i, idx) => theCorrectAnswerArray.includes(idx + 1));
+    const theCorrectAnswerArray = q["ĐÁP ÁN ĐÚNG"]
+      .toString()
+      .replaceAll(" ", "")
+      .split(",")
+      .map((i) => parseInt(i));
+    const correctBoolean = [false, false, false, false].map((i, idx) =>
+      theCorrectAnswerArray.includes(idx + 1)
+    );
     console.log(correctBoolean, q["ĐÁP ÁN ĐÚNG"]);
     // console.log(correctBoolean, q.answer);
 
-    if (correctBoolean.every((i, idx) => i === q.answer[idx])){
+    if (correctBoolean.every((i, idx) => i === q.answer[idx])) {
       return acc + 1;
     }
     return acc;
   }, 0);
 
-  console.log('result', result);
+  console.log("result", result);
+
+  useEffect(() => {
+    const _q = loadQuestion();
+    setQuestionIdx(0);
+    setQuestions(_q);
+  }, []);
 
   return (
     <div className="p-20 text-black">
-      <button onClick={() => {
-        setQuestions(loadQuestion());
-        setQuestionIdx(0);
-      }} className={`${secondaryBtnClassName}`}>Thi Lai</button>
+      <button
+        onClick={() => {
+          setQuestions(loadQuestion());
+          setQuestionIdx(0);
+        }}
+        className={`${secondaryBtnClassName}`}
+      >
+        Thi Lai
+      </button>
       <div>
         {currentQuestIdx < questions.length && currentQuestIdx > -1 ? (
-          <>
-            <h1>Question number: {currentQuestIdx + 1}</h1>
-            <Question onChange={q => {
+          <div className="flex flex-col gap-4">
+            <h1>Câu hỏi {`${currentQuestIdx + 1}/100`}</h1>
+            <Question
+              onChange={(q) => {
                 const newQuestions = questions.map((i, idx) => {
-                    if (idx === currentQuestIdx) {
-                        return q;
-                    }
-                    return i;
-                })
+                  if (idx === currentQuestIdx) {
+                    return q;
+                  }
+                  return i;
+                });
                 setQuestions(newQuestions);
-            }} question={questions[currentQuestIdx]} />
-            {
-              currentQuestIdx > 0 && <button className={`${secondaryBtnClassName}`} onClick={() => setQuestionIdx(currentQuestIdx - 1)}>
-              Câu Trước
-            </button> 
-            }
-            <button className={`${secondaryBtnClassName}`} onClick={() => setQuestionIdx(currentQuestIdx + 1)}>
-              Câu tiếp theo
-            </button>
-          </>
+              }}
+              question={questions[currentQuestIdx]}
+            />
+            <div className="flex justify-end">
+              {currentQuestIdx > 0 && (
+                <button
+                  className={`${secondaryBtnClassName}`}
+                  onClick={() => setQuestionIdx(currentQuestIdx - 1)}
+                >
+                  Câu Trước
+                </button>
+              )}
+              <button
+                className={`${secondaryBtnClassName}`}
+                onClick={() => setQuestionIdx(currentQuestIdx + 1)}
+              >
+                Câu tiếp theo
+              </button>
+            </div>
+          </div>
         ) : (
           <div>
             <p>Finish</p>
@@ -74,7 +102,8 @@ const Practice = () => {
   );
 };
 
-const secondaryBtnClassName = "mt-6 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+const secondaryBtnClassName =
+  "mt-6 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800";
 
 const Question = ({
   question,
@@ -84,98 +113,107 @@ const Question = ({
   onChange: (_q: Question) => void;
 }) => {
   return (
-    <>
-      <p>{question["Nội dung câu hỏi"]}</p>
-      <div>
-        <input
-          onChange={(e) =>
-            onChange({
-              ...question,
-              answer: [
-                e.target.checked,
-                question.answer[0],
-                question.answer[2],
-                question.answer[3],
-              ],
-            })
-          }
-          type="checkbox"
-          id="option1"
-          name="option1"
-          checked={question.answer[0]}
-        //   value={question.answer[0] ? "true" : "false"}
-        />
-        <label className="text-black" htmlFor="option1">{question["Câu trả lời 1"]}</label>
+    <div>
+      <p className="mb-6">{question["Nội dung câu hỏi"]}</p>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-2">
+          <input
+            onChange={(e) =>
+              onChange({
+                ...question,
+                answer: [
+                  e.target.checked,
+                  question.answer[0],
+                  question.answer[2],
+                  question.answer[3],
+                ],
+              })
+            }
+            type="checkbox"
+            id="option1"
+            name="option1"
+            checked={question.answer[0]}
+          />
+          <label className="text-black" htmlFor="option1">
+            {question["Câu trả lời 1"]}
+          </label>
+        </div>
+        <div className="flex gap-2">
+          <input
+            type="checkbox"
+            id="option2"
+            name="option2"
+            //   value={question.answer[1] ? "true" : "false"}
+            checked={question.answer[1]}
+            onChange={(e) =>
+              onChange({
+                ...question,
+                answer: [
+                  question.answer[0],
+                  e.target.checked,
+                  question.answer[2],
+                  question.answer[3],
+                ],
+              })
+            }
+          />
+          <label htmlFor="option2">{question["Câu trả lời 2"]}</label>
+        </div>
+        <div className="flex gap-2">
+          <input
+            type="checkbox"
+            id="option3"
+            name="option3"
+            //   value={question.answer[2] ? "true" : "false"}
+            checked={question.answer[2]}
+            onChange={(e) =>
+              onChange({
+                ...question,
+                answer: [
+                  question.answer[0],
+                  question.answer[1],
+                  e.target.checked,
+                  question.answer[3],
+                ],
+              })
+            }
+          />
+          <label htmlFor="option3">{question["Câu trả lời 3"]}</label>
+        </div>
+        <div className="flex gap-2">
+          <input
+            type="checkbox"
+            id="option4"
+            name="option4"
+            //   value={question.answer[3] ? "true" : "false"}
+            checked={question.answer[3]}
+            onChange={(e) =>
+              onChange({
+                ...question,
+                answer: [
+                  question.answer[0],
+                  question.answer[1],
+                  question.answer[2],
+                  e.target.checked,
+                ],
+              })
+            }
+          />
+          <label htmlFor="option4">{question["Câu trả lời 4"]}</label>
+        </div>
       </div>
-      <div>
-        <input
-          type="checkbox"
-          id="option2"
-          name="option2"
-        //   value={question.answer[1] ? "true" : "false"}
-          checked={question.answer[1]}
-          onChange={(e) =>
-            onChange({
-              ...question,
-              answer: [
-                question.answer[0],
-                e.target.checked,
-                question.answer[2],
-                question.answer[3],
-              ],
-            })
-          }
-        />
-        <label htmlFor="option2">{question["Câu trả lời 2"]}</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          id="option3"
-          name="option3"
-        //   value={question.answer[2] ? "true" : "false"}
-          checked={question.answer[2]}
-          onChange={(e) =>
-            onChange({
-              ...question,
-              answer: [
-                question.answer[0],
-                question.answer[1],
-                e.target.checked,
-                question.answer[3],
-              ],
-            })
-          }
-        />
-        <label htmlFor="option3">{question["Câu trả lời 3"]}</label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          id="option4"
-          name="option4"
-        //   value={question.answer[3] ? "true" : "false"}
-          checked={question.answer[3]}
-          onChange={(e) =>
-            onChange({
-              ...question,
-              answer: [
-                question.answer[0],
-                question.answer[1],
-                question.answer[2],
-                e.target.checked,
-              ],
-            })
-          }
-        />
-        <label htmlFor="option4">{question["Câu trả lời 4"]}</label>
-      </div>
-    </>
+    </div>
   );
 };
 
 const loadQuestion = (): Question[] => {
-  return data.map((i) => ({ ...i, answer: [false, false, false, false] }));
+  // Shuffle array
+  const shuffled = data.sort(() => 0.5 - Math.random());
+
+  // Get sub-array of first 100 elements after shuffled
+  let selected = shuffled.slice(0, 100);
+
+  return selected.map((i) => ({ ...i, answer: [false, false, false, false] }));
 };
 
 export default Practice;
