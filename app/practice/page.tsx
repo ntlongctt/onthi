9e90d8d19ad8217c096b556f6b585c01d49ede0e
longnguyen <longnguyenctt@gmail.com>
2024-02-18 15:39:25 +0700

    Add Link component and update button text

--- app/practice/page.tsx
+++ app/practice/page.tsx
@@ -50,8 +50,8 @@ const Practice = () => {
                 })
                 setQuestions(newQuestions);
             }} question={questions[currentQuestIdx]} />
-            <button onClick={() => setQuestionIdx(currentQuestIdx + 1)}>
-              Next
+            <button className="mt-6 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800" onClick={() => setQuestionIdx(currentQuestIdx + 1)}>
+              Câu tiếp theo
             </button>
           </>
         ) : (
